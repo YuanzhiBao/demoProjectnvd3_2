a929ad15c30ed4255e7ad9f8d66f6b5096dafe00
YuanzhiBao <baoyu2017@gmail.com>
2018-03-23 15:21:44 -0400

    second commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\byz10\PycharmProjects\demoProjectnvd3_2\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
